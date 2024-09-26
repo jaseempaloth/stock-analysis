f036ca3e9532674829ed57751679f1f1f64134df
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-09-27 01:00:39 +0530

    Refactor app.py to add download buttons for filtered data

--- app.py
+++ app.py
@@ -21,8 +21,24 @@ if uploaded_file is not None:
     st.subheader("Open High")
     st.dataframe(open_high)
 
+    # Download filtered data as CSV
+    st.download_button(
+        label="Download Open High Data",
+        data=open_high.to_csv(index=False),
+        file_name='open_high.csv',
+        mime='text/csv'
+    )
+
     st.subheader("Open Low")
     st.dataframe(open_low)
 
+    st.download_button(
+        label="Download Open Low Data",
+        data=open_low.to_csv(index=False),
+        file_name='open_low.csv',
+        mime='text/csv'
+    )
+
 else:
     st.subheader("Please upload your csv file to proceed")
+
